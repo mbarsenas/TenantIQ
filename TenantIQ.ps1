35ef11991ee75122adc0be416813f60813f1a0c8
Mark <mbarsenas@gmail.com>
2026-08-16 02:57:01 -0500

    Restore persistent workload submenus

--- TenantIQ.ps1
+++ TenantIQ.ps1
@@ -1,4 +1,4 @@
-﻿$FrameworkPath = Join-Path $PSScriptRoot "01 Framework"
+$FrameworkPath = Join-Path $PSScriptRoot "01 Framework"
 $ModulesPath   = Join-Path $PSScriptRoot "10 Modules"
 
 # ============================================================
@@ -322,12 +322,125 @@ function Ensure-TenantIQSharePointConnection {
     } catch { Write-Host '';Write-Host 'Could not connect to SharePoint Online.' -ForegroundColor Red;Write-Host $_.Exception.Message -ForegroundColor Red;return $false }
 }
 
-function Start-TenantIQSharePointModule { if(-not(Ensure-TenantIQSharePointConnection)){Wait-TenantIQ;return}; Start-TenantIQSharePointAssessment; Wait-TenantIQ }
-function Start-TenantIQTeamsModule { Start-TenantIQTeamsAssessment; Wait-TenantIQ }
-function Start-TenantIQOneDriveModule { if(-not(Ensure-TenantIQSharePointConnection)){Wait-TenantIQ;return}; Start-TenantIQOneDriveAssessment; Wait-TenantIQ }
-function Start-TenantIQIntuneModule { Start-TenantIQIntuneAssessment; Wait-TenantIQ }
-function Start-TenantIQDefenderModule { Start-TenantIQDefenderAssessment; Wait-TenantIQ }
-function Start-TenantIQPurviewModule { Start-TenantIQPurviewAssessment; Wait-TenantIQ }
+function Start-TenantIQSharePointModule {
+    while ($true) {
+        Show-Banner
+        Write-Host 'SharePoint Online' -ForegroundColor Cyan
+        Write-Host '[1] Full SharePoint Online Assessment'
+        Write-Host '[2] Health Checks'
+        Write-Host '[0] Back to Modules'
+        Write-Host ''
+        switch (Read-Host 'Select') {
+            '1' {
+                if (Ensure-TenantIQSharePointConnection) { Start-TenantIQSharePointAssessment }
+                Wait-TenantIQ
+            }
+            '2' {
+                foreach ($Check in ($TenantIQSharePointHealthChecks | Sort-Object { [int]$_.Number })) { Write-Host ("[{0}] {1}" -f $Check.Number,$Check.Name) }
+                Wait-TenantIQ
+            }
+            '0' { return }
+        }
+    }
+}
+
+function Start-TenantIQTeamsModule {
+    while ($true) {
+        Show-Banner
+        Write-Host 'Microsoft Teams' -ForegroundColor Cyan
+        Write-Host '[1] Full Microsoft Teams Assessment'
+        Write-Host '[2] Health Checks'
+        Write-Host '[0] Back to Modules'
+        Write-Host ''
+        switch (Read-Host 'Select') {
+            '1' { Start-TenantIQTeamsAssessment; Wait-TenantIQ }
+            '2' {
+                foreach ($Check in ($TenantIQTeamsHealthChecks | Sort-Object { [int]$_.Number })) { Write-Host ("[{0}] {1}" -f $Check.Number,$Check.Name) }
+                Wait-TenantIQ
+            }
+            '0' { return }
+        }
+    }
+}
+
+function Start-TenantIQOneDriveModule {
+    while ($true) {
+        Show-Banner
+        Write-Host 'OneDrive' -ForegroundColor Cyan
+        Write-Host '[1] Full OneDrive Assessment'
+        Write-Host '[2] Health Checks'
+        Write-Host '[0] Back to Modules'
+        Write-Host ''
+        switch (Read-Host 'Select') {
+            '1' {
+                if (Ensure-TenantIQSharePointConnection) { Start-TenantIQOneDriveAssessment }
+                Wait-TenantIQ
+            }
+            '2' {
+                foreach ($Check in ($TenantIQOneDriveHealthChecks | Sort-Object { [int]$_.Number })) { Write-Host ("[{0}] {1}" -f $Check.Number,$Check.Name) }
+                Wait-TenantIQ
+            }
+            '0' { return }
+        }
+    }
+}
+
+function Start-TenantIQIntuneModule {
+    while ($true) {
+        Show-Banner
+        Write-Host 'Microsoft Intune' -ForegroundColor Cyan
+        Write-Host '[1] Full Microsoft Intune Assessment'
+        Write-Host '[2] Health Checks'
+        Write-Host '[0] Back to Modules'
+        Write-Host ''
+        switch (Read-Host 'Select') {
+            '1' { Start-TenantIQIntuneAssessment; Wait-TenantIQ }
+            '2' {
+                foreach ($Check in ($TenantIQIntuneHealthChecks | Sort-Object { [int]$_.Number })) { Write-Host ("[{0}] {1}" -f $Check.Number,$Check.Name) }
+                Wait-TenantIQ
+            }
+            '0' { return }
+        }
+    }
+}
+
+function Start-TenantIQDefenderModule {
+    while ($true) {
+        Show-Banner
+        Write-Host 'Microsoft Defender' -ForegroundColor Cyan
+        Write-Host '[1] Full Microsoft Defender Assessment'
+        Write-Host '[2] Health Checks'
+        Write-Host '[0] Back to Modules'
+        Write-Host ''
+        switch (Read-Host 'Select') {
+            '1' { Start-TenantIQDefenderAssessment; Wait-TenantIQ }
+            '2' {
+                foreach ($Check in ($TenantIQDefenderHealthChecks | Sort-Object { [int]$_.Number })) { Write-Host ("[{0}] {1}" -f $Check.Number,$Check.Name) }
+                Wait-TenantIQ
+            }
+            '0' { return }
+        }
+    }
+}
+
+function Start-TenantIQPurviewModule {
+    while ($true) {
+        Show-Banner
+        Write-Host 'Microsoft Purview' -ForegroundColor Cyan
+        Write-Host '[1] Full Microsoft Purview Assessment'
+        Write-Host '[2] Health Checks'
+        Write-Host '[0] Back to Modules'
+        Write-Host ''
+        switch (Read-Host 'Select') {
+            '1' { Start-TenantIQPurviewAssessment; Wait-TenantIQ }
+            '2' {
+                foreach ($Check in ($TenantIQPurviewHealthChecks | Sort-Object { [int]$_.Number })) { Write-Host ("[{0}] {1}" -f $Check.Number,$Check.Name) }
+                Wait-TenantIQ
+            }
+            '0' { return }
+        }
+    }
+}
 
 function Show-Banner {
     Clear-Host
@@ -393,3 +506,4 @@ while($true){
     }
     if($Choice -eq '0'){break}
 }
+
